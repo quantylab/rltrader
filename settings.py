641cf24b22b99edb00be048f853c7363af2df89b
UnKuk Jeong <45334819+jukyellow@users.noreply.github.com>
2021-10-19 07:19:37 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # 로케일 설정
-if 'Linux' in platform.system() or 'Darwin' in platform.system():
-    locale.setlocale(locale.LC_ALL, 'ko_KR.UTF-8')
-elif 'Windows' in platform.system():
-    locale.setlocale(locale.LC_ALL, '')
+try:
+    if 'Linux' in platform.system() or 'Darwin' in platform.system():
+        locale.setlocale(locale.LC_ALL, 'ko_KR.UTF-8')
+    elif 'Windows' in platform.system():
+        locale.setlocale(locale.LC_ALL, '')
+except Exception as e:
+    print('[settings] e:', e)
